f833d3c70a7a3a003e020821c6193ce77301fbd5
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-14 17:34:30 +0600

    Create route.js

---
+++ app/auth/callback/route.js
@@ -0,0 +1,20 @@
+import { createClient } from '@/lib/supabase'
+import { NextResponse } from 'next/server'
+
+export async function GET(request) {
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
+  const next = requestUrl.searchParams.get('next') || '/dashboard'
+
+  if (code) {
+    const supabase = createClient()
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    
+    if (!error) {
+      return NextResponse.redirect(new URL(next, request.url))
+    }
+  }
+
+  // Return the user to an error page with instructions
+  return NextResponse.redirect(new URL('/auth/auth-code-error', request.url))
+}
